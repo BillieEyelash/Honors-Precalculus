fa302c2a969c36828f49a6cfdd3801e4d6e24c0e
BillieEyelash <riatalwar@hotmail.com>
2021-10-12 12:29:59 -0700

    Bad, basic grade reporter

---
+++ Unit-1/GradeReporter.py
@@ -0,0 +1,19 @@
+score = int(input("Enter your score on the test: "))
+if score > 93:
+    print("A")
+elif score > 90:
+    print("A-")
+elif score > 87:
+    print("B+")
+elif score > 83:
+    print("B")
+elif score > 80:
+    print("B-")
+elif score > 77:
+    print("C+")
+elif score > 73:
+    print("C")
+elif score > 70:
+    print("C-")
+else:
+    print("E")
